8ad5240bc0dd5e99f5434a61bcf40c324541a194
Doanda37Rahma <ddoanda@gmail.com>
2023-05-29 09:33:02 +0700

    feat: init login and register

---
+++ app/src/main/java/com/doanda/easymeal/ui/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.doanda.easymeal.ui.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.doanda.easymeal.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/doanda/easymeal/ui/register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.doanda.easymeal.ui.register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.doanda.easymeal.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
