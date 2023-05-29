1b9c6ae5ea7a8690de602ce96c229f11a4d6da2a
Doanda37Rahma <ddoanda@gmail.com>
2023-05-29 08:34:55 +0700

    chore: resolve merge conflict

--- app/src/main/java/com/doanda/easymeal/MainActivity.kt
+++ app/src/main/java/com/doanda/easymeal/MainActivity.kt
@@ -1,22 +1,14 @@
 package com.doanda.easymeal
 
-<<<<<<< HEAD
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
+import com.doanda.easymeal.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-=======
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import com.doanda.easymeal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -40,6 +32,5 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
->>>>>>> origin/master
     }
 }
