28631ca39e022e951114845c7a8287fa94328a46
Doanda37Rahma <ddoanda@gmail.com>
2023-06-14 03:56:25 +0700

    feat: add splash screen

--- app/src/main/java/com/doanda/easymeal/MainActivity.kt
+++ app/src/main/java/com/doanda/easymeal/MainActivity.kt
@@ -29,6 +29,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        setTheme(R.style.Theme_EasyMeal)
+
         hideSystemUI()
 
         setupData()
@@ -40,34 +42,26 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupData() {
+        setupView()
         viewModel.getLoginStatus().observeOnce(this) { isLogin ->
             if (!isLogin) {
                 goToLogin()
             }
         }
-        setupView()
-//        binding.fabCamera.setOnClickListener {
-//            goToSetting()
-//        }
-    }
-
-    private fun goToLogin() {
-        Toast.makeText(this, "Main -> Login", Toast.LENGTH_SHORT).show()
-        val intent = Intent(this@MainActivity, LoginActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
-        finish()
-        startActivity(intent)
-    }
-
-    private fun goToSetting() {
-        Toast.makeText(this, "Main -> Setting", Toast.LENGTH_SHORT).show()
-        val intent = Intent(this@MainActivity, SettingActivity::class.java)
-        startActivity(intent)
+        viewModel.getUser().observeOnce(this) { user ->
+            if (user.userEmail != "null") {
+                binding.tvUserEmail.text = user.userEmail
+            }
+        }
+        viewModel.getUserName().observeOnce(this) { name ->
+            if (name != "null") {
+                binding.tvUserName.text = name
+            }
+        }
     }
 
     private fun setupView() {
         Toast.makeText(this, "Main setup view", Toast.LENGTH_SHORT).show()
-
 
         val navView: BottomNavigationView = binding.navView
         navView.background = null
@@ -88,9 +82,31 @@ class MainActivity : AppCompatActivity() {
         navView.setupWithNavController(navController)
 
         binding.fabCamera.setOnClickListener {
-            val intentToDetection = Intent(this@MainActivity, DetectionActivity::class.java)
-            startActivity(intentToDetection)
+            goToDetection()
         }
+        binding.ivUserIcon.setOnClickListener {
+            goToSetting()
+        }
+    }
+
+    private fun goToLogin() {
+        Toast.makeText(this, "Main -> Login", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this@MainActivity, LoginActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
+        finish()
+        startActivity(intent)
+    }
+
+    private fun goToSetting() {
+        Toast.makeText(this, "Main -> Setting", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this@MainActivity, SettingActivity::class.java)
+        startActivity(intent)
+    }
+
+
+    private fun goToDetection() {
+        val intent = Intent(this@MainActivity, DetectionActivity::class.java)
+        startActivity(intent)
     }
 
     private fun hideSystemUI() {
@@ -105,6 +121,4 @@ class MainActivity : AppCompatActivity() {
         }
         supportActionBar?.hide()
     }
-
-
 }
